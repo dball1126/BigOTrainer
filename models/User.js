20025846d94a07dbefcd070a5bf405a334042b37
daniel <g.thirteen333@gmail.com>
2019-08-14 17:16:35 -0400

    user model complete

---
+++ models/User.js
@@ -0,0 +1,27 @@
+const mongoose = reuqire('mongoose');
+const Schema = mongoose.Schema;
+
+const UserSchema = new Schema({
+    handle: {
+        type: String,
+        required: true
+    },
+    email: {
+        type: String,
+        required: true
+    },
+    username: {
+        type: String,
+        required: true
+    },
+    password: {
+        type: String,
+        required: true
+    },
+    date: {
+        type: Date,
+        default: Date.now
+    }
+})
+
+module.expres = User = mongoose.model('users', UserSchema);
