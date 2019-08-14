a3175847cff8cf2bba2f59e3196b533a7892ab4a
daniel <g.thirteen333@gmail.com>
2019-08-14 16:24:21 -0400

    mongo setup and connected successfully

--- app.js
+++ app.js
@@ -1,6 +1,12 @@
 const express = require("express");
 const app = express();
-app.get("/", (req, res) => res.send("Hello World"));
-const port = process.env.PORT || 5000;
+const mongoose = require('mongoose');
 
+app.get("/", (req, res) => res.send("Hello Baby"));
+const db = require('./config/keys').mongoURI;
+const port = process.env.PORT || 5000;
+mongoose
+    .connect(db, {useNewUrlParser: true})
+    .then(() => console.log("Connectged to MongoDB successfully"))
+    .catch(err => console.log(err));
 app.listen(port, () => console.log(`Server is running on port ${port}`));
