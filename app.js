61523bde19cd76dac840e911689504eb82736caa
daniel <g.thirteen333@gmail.com>
2019-08-14 17:03:03 -0400

    express router setup complete

--- app.js
+++ app.js
@@ -3,13 +3,16 @@ const app = express();
 const mongoose = require('mongoose');
 const users = require('./routes/api/users');
 const quizes = require('./routes/api/quizes');
+const bodyParser = require('body-parser');
+const db = require('./config/keys').mongoURI;
+const port = process.env.PORT || 5000;
 
 app.get("/", (req, res, next) => res.send("Hello Baby"));
 app.use("/api/users", users);
 app.use("/api/quizes", quizes);
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
 
-const db = require('./config/keys').mongoURI;
-const port = process.env.PORT || 5000;
 mongoose
     .connect(db, {useNewUrlParser: true})
     .then(() => console.log("Connectged to MongoDB successfully"))
