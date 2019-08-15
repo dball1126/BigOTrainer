15e836aa2ac945014c03b238619c55b46b3a3668
daniel <g.thirteen333@gmail.com>
2019-08-15 15:30:52 -0400

    user posts successfully

--- app.js
+++ app.js
@@ -8,10 +8,10 @@ const db = require('./config/keys').mongoURI;
 const port = process.env.PORT || 5000;
 
 app.get("/", (req, res, next) => res.send("Hello Baby"));
-app.use("/api/users", users);
-app.use("/api/quizes", quizes);
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
+app.use("/api/users", users);
+app.use("/api/quizes", quizes);
 
 mongoose
     .connect(db, {useNewUrlParser: true})
