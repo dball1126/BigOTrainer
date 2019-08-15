dfceb874a3b3b860bcdae03e3b09cc3dcbf39d6b
daniel <g.thirteen333@gmail.com>
2019-08-15 14:20:25 -0400

    quiz models complete

--- routes/api/users.js
+++ routes/api/users.js
@@ -1,7 +1,27 @@
 const express = require("express");
 const router = express.Router();
-
+const bcrypt = require('bcryptjs');
+const User = require('../../models/User');
 
 router.get("/test", (req, res) => res.json({ msg: "This is the users route" }));
+
+router.post('/register', (req, res) => {
+    User.findOne({ email: req.body.email })
+        .then(user => {
+            if (user) {
+                return res.status(400).json({email: "A user has already registered with this address"})
+            } else {
+                const newUser = new User({
+                    username: req.body.handle,
+                    email: req.body.email,
+                    password: req.body.password
+                })
+
+                bcrypt.genSalt(10, (err, salt) => {
+                    bcrypt.hash()
+                })
+            }
+        })
+})
 
 module.exports = router;
