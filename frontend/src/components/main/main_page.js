9a4fa09f43ab2a697ab338d4448c2e7acf52fe36
daniel <g.thirteen333@gmail.com>
2019-08-17 20:26:01 -0400

    main page component created

---
+++ frontend/src/components/main/main_page.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+class MainPage extends React.Component {
+    render(){
+        return (
+            <div>
+                <h1>BigOTrainer</h1>
+                <footer>
+                    Footer
+                </footer>
+            </div>
+        )
+    }
+}
+
+export default MainPage;
