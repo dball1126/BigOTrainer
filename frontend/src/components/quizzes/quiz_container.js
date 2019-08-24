ad138360c057c3b528217315e8d1be2a4aebb46f
daniel <g.thirteen333@gmail.com>
2019-08-24 00:32:07 -0400

    quiz container complete

---
+++ frontend/src/components/quizzes/quiz_container.js
@@ -0,0 +1,17 @@
+import { connect } from 'react-redux';
+import { fetchQuizzes } from '../../actions/quiz_actions';
+import Quizzes from './quizzes';
+
+const mapStateToProps = (state) => {
+    return {
+        quizzes: Object.values(state.quizzes.all)
+    }
+}
+
+const mapDispatchToProps = (dispatch) => {
+    return {
+        fetchQuizzes: () => dispatch(fetchQuizzes())
+    }
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(Quizzes);
