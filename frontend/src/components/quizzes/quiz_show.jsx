1ebbe550d927517ebfcafc6cb58570742689eecd
daniel <g.thirteen333@gmail.com>
2019-08-27 03:28:00 -0400

    shell of quiz show component complete

--- frontend/src/components/quizzes/quiz_show.jsx
+++ frontend/src/components/quizzes/quiz_show.jsx
@@ -1,4 +1,23 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
+import { Link, withRouter } from 'react-router-dom';
+import NavBar from '../nav/navbar_container';
+ class QuizShow extends React.Component{
+     constructor(props){
+         super(props);
+         this.state = {quiz: ""}
+     }
+     componentWillMount(){
+         
+     }
 
- 
+     render(){
+
+        return (
+            <div>
+                <h1>Quiz Show Component</h1>
+            </div>
+        )
+     }
+ }
+
+ export default withRouter(QuizShow);
