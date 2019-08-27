2ba27fcecd4f1b9ceac0acbcdc35b5371d5101d8
daniel <g.thirteen333@gmail.com>
2019-08-27 00:33:10 -0400

    update to quiz button

--- frontend/src/components/quizzes/quiz_list_item.jsx
+++ frontend/src/components/quizzes/quiz_list_item.jsx
@@ -1,12 +1,15 @@
 import React from 'react';
+import {Link} from 'react-router-dom';
 
-const QuizListItem = ({name, level}) => {
+const QuizListItem = ({key, name, level}) => {
       
         return (
             <div className="quiz-item">
                 <h3 className="quiz-name">{name}</h3>
                 <h3>{level}</h3>
-                <button className="begin-quiz">Begin</button>
+                <div className="begin-quiz">
+                    <Link to={`/quizzes/${key}`} className="begin-quiz-button">Begin</Link>
+                </div>
             </div>
         )
 }
