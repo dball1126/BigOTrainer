2047f5b0a4f25755447b5cfefe25288c504285c2
daniel <g.thirteen333@gmail.com>
2019-09-29 07:16:23 -0400

    ready for merge

--- frontend/src/components/app.js
+++ frontend/src/components/app.js
@@ -2,14 +2,21 @@ import React from 'react';
 import { AuthRoute, ProtectedRoute } from '../util/route_util';
 import { Switch } from 'react-router-dom';
 import MainPage from './main/main_page';
-
+const path = require('path')
 import LoginFormContainer from './session/login_form_container';
 import SignupFormContainer from './session/signup_form_container';
 import QuizzesContainer from './quizzes/quiz_container';
 import QuizShowContainer from './quizzes/quiz_show_container';
-
+if (process.env.NODE_ENV === 'production') {
+    app.use(express.static('frontend/build'));
+    app.get('/', (req, res) => {
+        res.sendFile(path.resolve(__dirname, 'frontend', 'build', 'index.html'));
+    })
+}
 const App = () => (
     <div>
+        
+
         <Switch>
             <AuthRoute exact path="/" component={MainPage} />
             <AuthRoute exact path="/login" component={LoginFormContainer} />
