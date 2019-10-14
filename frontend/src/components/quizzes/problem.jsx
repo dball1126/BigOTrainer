b3a1b85ed06cb4db69ca9de2fe22bae685bfa36e
daniel <g.thirteen333@gmail.com>
2019-10-14 18:37:41 -0400

    update to render last element in problem line

--- frontend/src/components/quizzes/problem.jsx
+++ frontend/src/components/quizzes/problem.jsx
@@ -3,11 +3,12 @@ import { darcula } from 'react-syntax-highlighter/dist/esm/styles/prism';
 import React from 'react';
 
 const Problem = ({problem}) => {
-
+    let length = problem.split(",").length-1;
+    
     return (
         <>
             {problem.split(",").map((line, i) => (
-                <div className={`pre${i}`} id={"pre-problem"} key={i}>
+                <div className={`pre${i}`} id={length === i ? "pre-problem-last" : "pre-problem"} key={i}>
                     <SyntaxHighlighter language="javascript" style={darcula} id={`star${line}`} >
                         {line}
                     </SyntaxHighlighter>
