7df24e28cba0d6cc01d6a1c2c95bf2ac229cf9f2
daniel <g.thirteen333@gmail.com>
2019-10-14 19:37:34 -0400

    update to begin quiz button link

--- frontend/src/components/quizzes/quiz_list_item.jsx
+++ frontend/src/components/quizzes/quiz_list_item.jsx
@@ -11,9 +11,11 @@ const QuizListItem = ({name, level, id, quizzesWon, quizzesLost, userEmail, time
                     <h3 className="quiz-level">Level: {level}</h3>
                     <h3 className="times-taken">Times taken: {timesTaken}</h3>
                 </div>
+                    <Link to={{ pathname: `/quizzes/${id}`, quizzesWon: quizzesWon , quizzesLost: quizzesLost , userEmail: userEmail }} className="begin-quiz-button" >
                 <div className="begin-quiz">
-                    <Link to={{ pathname: `/quizzes/${id}`, quizzesWon: quizzesWon , quizzesLost: quizzesLost , userEmail: userEmail }} className="begin-quiz-button" >Begin</Link>
+                    Begin
                 </div>
+                    </Link>
             </div>
         )
 }
