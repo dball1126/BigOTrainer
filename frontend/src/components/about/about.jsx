6b15f3daedd037828d36a956eb707d35629a616c
daniel <g.thirteen333@gmail.com>
2019-10-16 00:24:18 -0400

    about body section added and styled

--- frontend/src/components/about/about.jsx
+++ frontend/src/components/about/about.jsx
@@ -6,7 +6,7 @@ const About = () => {
     return (
         <div className="homepage-container">
             <NavBar />
-            <div className="homepage-body">
+            <div className="about-body">
                 <div className="about-title">
                     <p>About</p>
                 </div>
@@ -17,7 +17,7 @@ const About = () => {
                         Big O notation is a mathematical notation that is used on this website to help train you to become better at
                   finding the Time Complexity of an algorithm. For example: If you are given a number equal to 10 and you have to print
                   out every number, this is referred to as Constant Time( O(1) ), but if you do not know what that number is then the
-                  notation would be referred to as Linear Time( O(n) ). You can refer to the <a href="https://www.bigocheatsheet.com/" target="_blank">Big-O Cheat Sheet</a>
+                  notation would be referred to as Linear Time( O(n) ). You can refer to the <a href="https://www.bigocheatsheet.com/" target="_blank">Big-O Cheat Sheet </a> 
                   for more details.
                 </p>
             </div>
