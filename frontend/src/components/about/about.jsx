ca40478bf4aa66e97d9a7c829875540681f92da6
daniel <g.thirteen333@gmail.com>
2019-10-15 23:41:43 -0400

    about page added to frontend and renders

--- frontend/src/components/about/about.jsx
+++ frontend/src/components/about/about.jsx
@@ -1,7 +1,17 @@
+import {Link} from 'react-router-dom';
+import NavBar from '../nav/navbar';
+import React from 'react';
+
 const About = () => {
     return (
-        <div>
-
+        <div className="homepage-container">
+            <NavBar />
+            <div className="homepage-body">
+            <span>Every algorithm has a run time analysis where you can get a big picture idea of how fast it will run.
+                  When the input is 10 billion versus 100 it becomes much more important to look at the worst case scenario,
+                  because it matters that much more.
+            </span>
+            </div>
         </div>    
     )
 }
--- frontend/src/components/app.js
+++ frontend/src/components/app.js
@@ -6,10 +6,12 @@ import SignupFormContainer from './session/signup_form_container';
 import QuizzesContainer from './quizzes/quiz_container';
 import QuizShowContainer from './quizzes/quiz_show_container';
 import MainPageContainer from './main/main_page_container'
+import About from './about/about';
 const App = () => (
     <div>
         <Switch>
             <AuthRoute exact path="/" component={MainPageContainer} />
+            <AuthRoute exact path="/about" component={About} />
             <AuthRoute exact path="/login" component={LoginFormContainer} />
             <AuthRoute exact path="/signup" component={SignupFormContainer} />
             <ProtectedRoute exact path="/quizzes/:quizId" component={QuizShowContainer} />
