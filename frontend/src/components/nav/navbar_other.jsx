e0b19efed1aacbd4dea0d452d60830d3bd9b8a9e
daniel <g.thirteen333@gmail.com>
2019-10-17 22:43:12 -0400

    link works for back arrow

--- frontend/src/components/nav/navbar_other.jsx
+++ frontend/src/components/nav/navbar_other.jsx
@@ -1,10 +1,11 @@
 import React from 'react';
+import {Link} from 'react-router-dom';
 
 const NavBarOther = () => {
     return (
         <div className="navbar-other-container">
             <div className="bigo-box">
-                <img src="https://i.imgur.com/emF2ura.png" />
+                <Link to={"/"}><img src="https://i.imgur.com/emF2ura.png" className="back-arrow" /></Link>
             </div>
         </div>
     );
