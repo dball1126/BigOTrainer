e28782e31df0fb49134933102276bf40d0cb8682
daniel <g.thirteen333@gmail.com>
2019-10-17 22:14:36 -0400

    times taken changed to attempts

--- frontend/src/components/quizzes/quiz_list_item.jsx
+++ frontend/src/components/quizzes/quiz_list_item.jsx
@@ -9,7 +9,7 @@ const QuizListItem = ({name, level, id, quizzesWon, quizzesLost, userEmail, time
                 <div className="quiz-details">
                     <h3 className="quiz-name">{name}</h3>
                     <h3 className="quiz-level">Level: {level}</h3>
-                    <h3 className="times-taken">Times taken: {timesTaken}</h3>
+                    <h3 className="times-taken">Attempts: {timesTaken}</h3>
                 </div>
                     <Link to={{ pathname: `/quizzes/${id}`, quizzesWon: quizzesWon , quizzesLost: quizzesLost , userEmail: userEmail }} className="begin-quiz-button" >
                 <div className="begin-quiz">
