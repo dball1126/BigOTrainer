fee80021cde4c0ed012e9574a83a1d3f54eaf5af
daniel <g.thirteen333@gmail.com>
2019-10-17 22:38:38 -0400

    back arrow url added for sessions

--- frontend/src/components/nav/navbar_other.jsx
+++ frontend/src/components/nav/navbar_other.jsx
@@ -3,7 +3,9 @@ import React from 'react';
 const NavBarOther = () => {
     return (
         <div className="navbar-other-container">
-
+            <div className="bigo-box">
+                <img src="https://i.imgur.com/emF2ura.png" />
+            </div>
         </div>
     );
 }
